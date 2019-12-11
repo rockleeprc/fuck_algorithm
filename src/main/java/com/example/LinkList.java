d80994d3825a822aad986dfa70586b46ec0923bf
rockleeprc <rockleeprc@outlook.com>
2019-12-11 20:57:43 +0800

    linklist

--- src/main/java/com/example/LinkList.java
+++ src/main/java/com/example/LinkList.java
@@ -11,6 +11,11 @@ public class LinkList<E> {
     private static class Node<E> {
         E element;
         Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
     }
 
     public void clear() {
@@ -18,7 +23,32 @@ public class LinkList<E> {
         first = null;
     }
 
-    public void add(int index,E elemnt){
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E oldValue = node.element;
+        node.element = element;
+        return oldValue;
+    }
+
+    public void add(int index, E element) {
+        if (index == 0) {
+            first.next = new Node<E>(element, first.next);
+        } else {
+            Node<E> prev = node(index - 1);
+            prev.next = new Node<E>(element, prev.next);
+        }
+        size++;
+    }
+
+    private Node<E> node(int index) {
+        Node node = first;
+        for (int i = 0; i < index; i++) {
+            node = first.next;
+        }
+        return node;
+    }
+
+    private void checkRange(int index) {
 
     }
 }
