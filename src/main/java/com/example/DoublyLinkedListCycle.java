2d43cfef06f99185f5479718caf2ccdd147205fb
rockleeprc <rockleeprc@outlook.com>
2019-12-20 20:37:00 +0800

    edit

---
+++ src/main/java/com/example/DoublyLinkedListCycle.java
@@ -0,0 +1,145 @@
+package com.example;
+
+public class DoublyLinkedListCycle<E> {
+
+    public static void main(String[] args) {
+        DoublyLinkedListCycle<String> linked = new DoublyLinkedListCycle<>();
+        linked.add("A");
+        linked.add("B");
+        linked.add("D");
+        linked.add("C");
+//        System.out.println(linked.size);
+//        System.out.println(linked.node(4));
+
+//        System.out.println(linked);
+//        linked.add(2, "b");
+//        System.out.println(linked);
+        System.out.println("remove=" + linked.remove(0));
+        System.out.println(linked);
+//        System.out.println("remove=" + linked.remove(2));
+//        System.out.println(linked);
+    }
+
+    private int size;
+    private Node<E> first;
+    private Node<E> last;
+
+    private static class Node<E> {
+        E element;
+        Node<E> prev;
+        Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
+            this.element = element;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "element=" + element +
+                    //", prev=" + prev +
+                    //", next=" + next +
+                    '}';
+        }
+    }
+
+    public void add(E element) {
+        add(size, element);
+    }
+
+    public void add(int index, E element) {
+        if (index == size) {// last添加
+            Node<E> oldLast = last;
+            last = new Node<>(oldLast, element, null);
+            if (oldLast == null) {// 链表添加的第一个元素
+                first = last;
+            } else {
+                oldLast.next = last;
+            }
+        } else {
+            Node<E> next = node(index);
+            Node<E> prev = next.prev;
+            Node node = new Node(prev, element, next);
+            next.prev = node;
+            if (prev == null) {// first添加 前驱为null
+                first = node;
+            } else {
+                prev.next = node;
+            }
+        }
+        size++;
+    }
+
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E oldValue = node.element;
+        node.element = element;
+        return oldValue;
+    }
+
+    public E remove(int index) {
+        Node<E> node = node(index);
+        E oldValue = node.element;
+        Node<E> prev = node.prev;
+        Node<E> next = node.next;
+
+        if(prev==null){ // index = 0
+            first  = next;
+        }else{
+            prev.next = next;
+        }
+
+        if(last==null){// index=size-1
+            last = prev;
+        }else{
+            next.prev = prev;
+        }
+
+        size--;
+        return oldValue;
+    }
+
+    public Node<E> node(int index) {
+        if (index < (index >> 1)) {
+            Node<E> node = first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            return node;
+        } else {
+            Node<E> node = last;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+            return node;
+        }
+    }
+
+    public void clear() {
+        size = 0;
+        first = null;
+        last = null;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node node = first;
+        for (int i = 0; i < size; i++) {
+            if (i != 0) sb.append("\r\n");
+            sb.append(node);
+            node = node.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/example/SinglyLinkedListCycle.java
@@ -0,0 +1,135 @@
+package com.example;
+
+public class SinglyLinkedListCycle<E> {
+    public static void main(String[] args) {
+        SinglyLinkedListCycle<String> list = new SinglyLinkedListCycle();
+        list.add("C");
+        list.add("D");
+        System.out.println(list);
+        list.add(2, "B");
+        System.out.println(list);
+        list.add(1, "b");
+        System.out.println(list);
+    }
+
+    private int size;
+    private Node<E> first;
+    private Node<E> last;
+
+    private static class Node<E> {
+        E element;
+        Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+    }
+
+    public void clear() {
+        size = 0;
+        first = null;
+    }
+
+    public E get(int index) {
+        checkRange(index);
+        return node(index).element;
+    }
+
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E oldValue = node.element;
+        node.element = element;
+        return oldValue;
+    }
+
+    public void add(E element) {
+        add(size, element);
+    }
+
+    public void add(int index, E element) {
+        checkRangeForAdd(index);
+        if (index == 0) {
+            first = new Node<E>(element, first);
+            last = size == 0 ? first : node(index - 1);
+            last.next = first;
+        } else {
+            Node<E> prev = node(index - 1);
+            prev.next = new Node<E>(element, prev.next);
+        }
+        size++;
+    }
+
+    public E remove(int index) {
+        Node<E> node = first;
+        if (index == 0) {
+            first = first.next;
+        } else {
+            Node prev = node(index - 1);
+            node = prev.next;
+            prev.next = node.next;// prev.next = prev.next.next;
+        }
+        size--;
+        return node.element;
+    }
+
+    public int indexOf(E element) {
+        if (element == null) {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (node.element.equals(null)) return i;
+                node = node.next;
+            }
+        } else {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (element.equals(node.element)) return i;
+                node = node.next;
+            }
+        }
+        return -1;
+    }
+
+    private Node<E> node(int index) {
+        checkRange(index);
+        Node node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    private void checkRangeForAdd(int index) {
+        if (index < 0 || index > size) {
+            indexOutOfBoundException(index);
+        }
+    }
+
+    private void checkRange(int index) {
+        if (index < 0 || index >= size) {
+            indexOutOfBoundException(index);
+        }
+    }
+
+    private void indexOutOfBoundException(int index) {
+        throw new IndexOutOfBoundsException("index = " + index + ",size=" + size);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node node = first;
+        for (int i = 0; i < size; i++) {
+            if (i != 0) sb.append(",");
+            sb.append(node.element);
+            node = node.next;
+        }
+
+        /*
+        while (node != null) {
+            node = node.next;
+        }
+         */
+        return sb.toString();
+    }
+}
