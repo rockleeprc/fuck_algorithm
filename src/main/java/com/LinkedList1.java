9ad2e1646a5521a88326a7087d3d176951ed8ee7
rockleeprc <rockleeprc@outlook.com>
2020-04-15 09:37:05 +0800

    拆分

--- src/main/java/com/LinkedList1.java
+++ src/main/java/com/LinkedList1.java
@@ -23,6 +23,28 @@ public class LinkedList1<E> {
         size++;
     }
 
+    public void update(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("size=" + size + ",index=" + index);
+        }
+
+        Node node = node(index);
+        node.element = e;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= index) {
+            throw new IndexOutOfBoundsException("size=" + size + ",index=" + index);
+        }
+
+        if (index == 0) {
+            first = first.next;
+        } else {
+            Node prev = node(index - 1);
+            prev.next = prev.next.next;
+        }
+    }
+
     private Node node(int index) {
         Node node = first;
         for (int i = 0; i < index; i++) {
@@ -67,9 +89,11 @@ public class LinkedList1<E> {
         list.add("a");
         list.add("c");
         list.add("d");
-        System.out.println(list);
         list.add(0, "x");
         list.add(4, "Q");
+        System.out.println(list);
+//        list.remove(4);
+        list.update(0,"AA");
         System.out.println(list);
     }
 }
