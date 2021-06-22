ff23335ad54b5646db1d468448003d2fcb8e7d64
rocklee <rockleeprc@outlook.com>
2021-06-22 17:58:54 +0800

    01

---
+++ src/main/java/com/leetcode/_1_两数之和.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1_两数之和 {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 13;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
--- src/main/java/com/leetcode/_20_有效的括号.java
+++ src/main/java/com/leetcode/_20_有效的括号.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -49,8 +51,29 @@ public class _20_有效的括号 {
         return stack.isEmpty();
     }
 
-    public static void main(String[] args) {
-        System.out.println(isValid2("{(])}"));
+    public static boolean isValid3(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            //碰到左括号，就把相应的右括号入栈
+            if (ch == '(') {
+                deque.push(')');
+            } else if (ch == '{') {
+                deque.push('}');
+            } else if (ch == '[') {
+                deque.push(']');
+            } else if (deque.isEmpty() || deque.peek() != ch) {
+                return false;
+            } else {//如果是右括号判断是否和栈顶元素匹配
+                deque.pop();
+            }
+        }
+        //最后判断栈中元素是否匹配
+        return deque.isEmpty();
+    }
 
+    public static void main(String[] args) {
+        System.out.println(isValid3("{()}"));
     }
 }
